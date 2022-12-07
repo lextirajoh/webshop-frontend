5f1c27f8ec4e74bcd130de449f36d57bc734cd34
Lex <alext@disroot.org>
2022-12-07 21:22:42 +0100

    Move ScrollToTop to components

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './utils/store';
-import ScrollToTop from './ScrollToTop';
+import ScrollToTop from './components/ScrollToTop';
 import App from './App';
 import './styles/main.css';
 import {
